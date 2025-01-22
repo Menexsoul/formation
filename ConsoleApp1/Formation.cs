87ef8b974ca02489ce02cd05507211c52c069ffa
Menexsoul(Thomas) <128993652+Menexsoul@users.noreply.github.com>
2025-01-22 11:47:52 +0100

    Add files via upload

---
+++ ConsoleApp1/Formation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Formation
+    {
+        private string code;
+        private string libelle;
+        private List<string> listSession;
+
+        public Formation(string unCode, string unLibelle)
+        {
+            this.code = unCode;
+            this.libelle = unLibelle;
+            this.listSession = new List<string>();
+        }
+
+        public void affecteParticipants(string Participant , int numeroSession)
+        {
+            
+        }
+
+        public void setCode(string unCode)
+        {
+            this.code = unCode;
+        }
+
+        public string getCode()
+        {
+            return this.code;
+        }
+
+        public void setLibelle(string unLibelle)
+        {
+            this.libelle = unLibelle;
+        }
+
+        public string getLibelle()
+        {
+            return this.libelle;
+        }
+
+        public void setMesSessions(List<string> uneSessions)
+        {
+            this.listSession = uneSessions;
+        }
+
+        public List<string> getMesSessions()
+        {
+            return this.listSession;
+        }
+    }
+
+   
+}
