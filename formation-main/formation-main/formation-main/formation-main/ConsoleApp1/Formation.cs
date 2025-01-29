60e49932a68ddbbee8583a1ac54fe8e116e889ce
Menexsoul(Thomas) <128993652+Menexsoul@users.noreply.github.com>
2025-01-29 12:01:13 +0100

    Add files via upload

---
+++ formation-main/formation-main/formation-main/formation-main/ConsoleApp1/Formation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Formation
+    {
+        private string code;
+        private string libelle;
+        private List<Session> mesSessions;
+
+        public Formation(string unCode, string unLibelle)
+        {
+            this.code = unCode;
+            this.libelle = unLibelle;
+            this.mesSessions = new List<Session>();
+        }
+
+        public void affecteParticipants(Participant p , int numeroSession)
+        {
+           foreach (Session session in mesSessions)
+           {
+                if (session.getNumero() == numeroSession)
+                {
+                    session.addParticipant(p);
+                }
+                else
+                {
+                    Console.WriteLine($"Session {numeroSession} introuvable.");
+                }
+           }
+        }
+
+        public void setCode(string unCode)
+        {
+            this.code = unCode;
+        }
+
+        public string getCode()
+        {
+            return this.code;
+        }
+
+        public void setLibelle(string unLibelle)
+        {
+            this.libelle = unLibelle;
+        }
+
+        public string getLibelle()
+        {
+            return this.libelle;
+        }
+
+        public void setMesSessions(List<Session> uneSessions)
+        {
+            this.mesSessions = uneSessions;
+        }
+
+        public List<Session> getMesSessions()
+        {
+            return this.mesSessions;
+        }
+    }
+
+   
+}
---
+++ formation-main/formation-main/formation-main/formation-main/ConsoleApp1/Session.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Session
+    {
+
+        private int numero;
+        private DateTime date;
+        private int nbMax;
+        private List<Participant> Participants;
+
+        public Session(int unNumero, DateTime uneDate, int unNbMax)
+        {
+            this.numero = unNumero;
+            this.date = uneDate;
+            this.nbMax = unNbMax;
+            this.Participants = new List<Participant>();
+        }
+
+        public void setNumero(int unNumero)
+        {
+            this.numero = unNumero;
+        }
+
+        public int getNumero()
+        {
+            return this.numero;
+        }
+
+        public void setDate(DateTime uneDate)
+        {
+            this.date = uneDate;
+        }
+
+        public DateTime getDate()
+        {
+            return this.date;
+        }
+
+        public void setNbMax(int unNbMax)
+        {
+            this.nbMax = unNbMax;
+        }
+
+        public int getNbMax()
+        {
+            return this.nbMax;
+        }
+
+        public void setParticipants(List<Participant> unParticipants)
+        {
+            this.Participants = unParticipants;
+        }
+
+        public List<Participant> getParticipants()
+        {
+            return this.Participants;
+        }
+
+        public bool estPleine()
+        {
+            return (nbMax != Participants.Count)? false: true;
+            /*if (nbMax != Participants.Count)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }*/
+        }
+
+        public void addParticipant(Participant p)
+        {
+            if (!estPleine())
+            {
+                Participants.Add(p);
+            }
+            else
+            {
+                Console.WriteLine($"Session {numero} est pleine");
+            }
+        }
+
+    }
+}
